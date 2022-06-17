f8f07edb4dd92c98c7c7964e2a835b6587932670
Andrew Adel <android8giga@gmail.com>
2022-06-17 12:10:58 +0200

    "1281. Subtract the Product and Sum of Digits of an Integer" Solution

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include "Problems_Sol_Functions/Number of 1 Bits.cpp"
+#include "Problems_Sol_Functions/Subtract the Product and Sum of Digits of an Integer.cpp"
 
 using namespace std;
 int main() {
     Solution sol;
-    uint32_t n= 00000000000000000000000000001011;
-    cout<<sol.hammingWeight(n);
+    uint32_t n= 234;
+    cout<<sol.subtractProductAndSum(n);
 
     return 0;
 }

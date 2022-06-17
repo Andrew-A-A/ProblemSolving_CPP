cd7e9d14c861d80bd71b0a9f3847b98d36b7ef53
Andrew Adel <android8giga@gmail.com>
2022-06-17 11:57:06 +0200

    "1523.Count Odd Numbers in an Interval Range" Solution

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Problems_Sol_Functions/Count Odd Numbers in an Interval Range.cpp"
+using namespace std;
+int main() {
+    Solution sol;
+    cout << sol.countOdds(3 , 7) << endl;
+    return 0;
+}

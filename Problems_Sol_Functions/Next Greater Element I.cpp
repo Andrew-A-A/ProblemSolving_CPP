a673adeed3b0802473a91d70dcf71c3d4a30cb7b
Andrew Adel <android8giga@gmail.com>
2022-06-18 14:30:07 +0200

    "496. Next Greater Element I" Solution

---
+++ Problems_Sol_Functions/Next Greater Element I.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        std::vector<int>::iterator it;
+        for (int i = 0; i < nums1.size(); ++i) {
+            it = find(nums2.begin(), nums2.end(), nums1[i]);
+            if (it==nums2.end()-1) {
+                ans.push_back(-1);
+                continue;
+            }
+            it.operator++();
+            if (*it>nums1[i]){
+                ans.push_back(*it);
+            } else {
+                bool fnd=false;
+                for (auto j = it; j < nums2.end() ; ++j) {
+                    if (*j>nums1[i]){
+                        ans.push_back(*j);
+                        fnd= true;
+                        break;
+
+                    }
+                }
+                if (!fnd)
+                ans.push_back(-1);
+            }
+        }
+        return ans;
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
-#include "Problems_Sol_Functions/Check If It Is a Straight Line.cpp"
+#include "Problems_Sol_Functions/Next Greater Element I.cpp"
 
 using namespace std;
+
+
+
 int main() {
     Solution sol;
-    vector<vector<int>> n={{0,0},{0,1},{0,-1}};
-    cout<<sol.checkStraightLine(n);
+    vector<int> n1={4,1,2},n2={1,3,4,2};
 
+    vector<int>ans =sol.nextGreaterElement(n1,n2);
+    for (int i = 0; i < ans.size(); ++i) {
+        cout<<ans[i]<<endl;
+    }
     return 0;
 }

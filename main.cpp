74f2a70cae12e3afb3c6acb5066788296b45267e
Andrew Adel <20201700157@cis.asu.edu.eg>
2023-05-11 16:20:48 +0300

    "1971. Find if Path Exists in Graph" Solution

--- main.cpp
+++ main.cpp
@@ -1,31 +1,11 @@
 #include <iostream>
-#include "Problems_Sol_Functions/Find the Town Judge.cpp"
+#include "Problems_Sol_Functions/Find Center of Star Graph.cpp"
 
 int main() {
     Solution sol;
-    vector<vector<int>> vect;
-    vector<int> v1;
-    v1.push_back(1);
-    v1.push_back(2);
-    vect.push_back(v1);
-
-    //vector<int> v2;
-    //v2.push_back(2);
-    //v2.push_back(3);
-    //vect.push_back(v2);
-
-   /* vector<int> v3;
-    v3.push_back(3);
-    v3.push_back(1);
-    vect.push_back(v3);*/
-  /*  for (int i = 0; i < 1; i++) {
-
-        for (int j = 0; j < 2; j++) {
-            vect
-        }
-
-    }*/
+    vector<vector<int>> vect {{1, 2}, {5, 1}, {1, 3}, {1, 4}};
    
-    cout << sol.findJudge(2, vect );
+   
+    cout << sol.findCenter(vect);
     return 0;
 }
